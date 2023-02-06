0aa7d5135695a2b6c28370da940580bdc07a1990
ext.azarubina <82389159+AnnaZarubinaa@users.noreply.github.com>
2023-02-06 11:46:58 +0200

    Added project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Анна Зарубина on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
